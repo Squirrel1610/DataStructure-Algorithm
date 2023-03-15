58a257fadb5165ff34b5b0cedabecbb7e826e4c7
Thinh Duy <thinh.nd.60cntt@ntu.edu.vn>
2023-03-15 14:54:10 +0700

    breadth first traversal

--- Data_Structures/Graph_Traversal/index.js
+++ Data_Structures/Graph_Traversal/index.js
@@ -74,6 +74,27 @@ class Graph{
 
         return result;
     }
+
+    breadthFirstTraversal(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while(queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            })
+        }
+
+        return result;
+    }
 }
 
 //init the graph
@@ -97,5 +118,6 @@ console.log(graph);
 
 // console.log(graph.depthFirstRecursive("A"));
 console.log(graph.depthFirstIterative("A"));
+console.log(graph.breadthFirstTraversal("A"));
 
 
